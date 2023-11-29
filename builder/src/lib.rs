5aeb1534ca3c038e8ae337a25584d09511879122
Syhan <liuxianhan@vip.qq.com>
2023-11-29 19:34:16 +0800

    做题，做debug的5,6,7,8题

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,19 +1,15 @@
 use proc_macro::TokenStream;
+use quote::ToTokens;
 use std::ops::Deref;
-use quote::{ToTokens};
-use syn::Expr;
 use syn::spanned::Spanned;
+use syn::Expr;
 
 #[proc_macro_derive(Builder, attributes(builder))]
 pub fn derive(input: TokenStream) -> TokenStream {
     let st = syn::parse_macro_input!(input as syn::DeriveInput);
     match do_expand(&st) {
-        Ok(ret) => {
-            ret.into()
-        }
-        Err(e) => {
-            e.to_compile_error().into()
-        }
+        Ok(ret) => ret.into(),
+        Err(e) => e.to_compile_error().into(),
     }
 }
 
@@ -59,31 +55,38 @@ fn do_expand(st: &syn::DeriveInput) -> syn::Result<proc_macro2::TokenStream> {
 }
 type StructFields = syn::punctuated::Punctuated<syn::Field, syn::Token![,]>;
 fn get_fields_from_derive_input(st: &syn::DeriveInput) -> syn::Result<&StructFields> {
-    if let syn::Data::Struct(syn::DataStruct{fields:syn::Fields::Named(syn::FieldsNamed{named, .. }), .. }) = &st.data {
+    if let syn::Data::Struct(syn::DataStruct {
+        fields: syn::Fields::Named(syn::FieldsNamed { named, .. }),
+        ..
+    }) = &st.data
+    {
         return Ok(named);
     }
     Err(syn::Error::new_spanned(st, "Must Define On Struct"))
 }
 fn generate_struct_fields(st: &syn::DeriveInput) -> syn::Result<proc_macro2::TokenStream> {
-    let fields  = get_fields_from_derive_input(st)?;
+    let fields = get_fields_from_derive_input(st)?;
     let idents: Vec<_> = fields.iter().map(|f| &f.ident).collect();
-    let types: syn::Result<Vec<_>> = fields.iter().map(|f| {
-        if let Some(inner_type) = get_generic_inner_type(&f.ty, "Option") {
-            Ok(quote::quote!(
-                std::option::Option<#inner_type>
-            ))
-        } else if get_field_user_specified_attrs(f)?.is_some() {
-            let origin_type = &f.ty;
-            Ok(quote::quote!(
-                #origin_type
-            ))
-        } else {
-            let original_type = &f.ty;
-            Ok(quote::quote!(
-                std::option::Option<#original_type>
-            ))
-        }
-    }).collect();
+    let types: syn::Result<Vec<_>> = fields
+        .iter()
+        .map(|f| {
+            if let Some(inner_type) = get_generic_inner_type(&f.ty, "Option") {
+                Ok(quote::quote!(
+                    std::option::Option<#inner_type>
+                ))
+            } else if get_field_user_specified_attrs(f)?.is_some() {
+                let origin_type = &f.ty;
+                Ok(quote::quote!(
+                    #origin_type
+                ))
+            } else {
+                let original_type = &f.ty;
+                Ok(quote::quote!(
+                    std::option::Option<#original_type>
+                ))
+            }
+        })
+        .collect();
 
     let types = types?;
     let ts = quote::quote!(
@@ -92,43 +95,47 @@ fn generate_struct_fields(st: &syn::DeriveInput) -> syn::Result<proc_macro2::Tok
     Ok(ts)
 }
 fn generate_struct_value(st: &syn::DeriveInput) -> syn::Result<Vec<proc_macro2::TokenStream>> {
-    let fields  = get_fields_from_derive_input(st)?;
-    let init_value: syn::Result<Vec<_>> = fields.iter().map(|f| {
-        let ident = &f.ident;
-        if get_field_user_specified_attrs(f)?.is_some() {
-            Ok(quote::quote!(
-                #ident: std::vec::Vec::new()
-            ))
-        } else {
-            Ok(quote::quote!(
-                #ident: std::option::Option::None
-            ))
-        }
-    }).collect();
-
+    let fields = get_fields_from_derive_input(st)?;
+    let init_value: syn::Result<Vec<_>> = fields
+        .iter()
+        .map(|f| {
+            let ident = &f.ident;
+            if get_field_user_specified_attrs(f)?.is_some() {
+                Ok(quote::quote!(
+                    #ident: std::vec::Vec::new()
+                ))
+            } else {
+                Ok(quote::quote!(
+                    #ident: std::option::Option::None
+                ))
+            }
+        })
+        .collect();
 
     Ok(init_value?)
 }
 
 fn generate_struct_setter(st: &syn::DeriveInput) -> syn::Result<proc_macro2::TokenStream> {
-    let fields  = get_fields_from_derive_input(st)?;
-    let idents: Vec<_> = fields.iter().map(|f|&f.ident).collect();
-    let tys: Vec<_> = fields.iter().map(|f|&f.ty).collect();
+    let fields = get_fields_from_derive_input(st)?;
+    let idents: Vec<_> = fields.iter().map(|f| &f.ident).collect();
+    let tys: Vec<_> = fields.iter().map(|f| &f.ty).collect();
 
     let mut final_token_stream = proc_macro2::TokenStream::new();
 
     for (idx, (ident, ty)) in idents.iter().zip(tys.iter()).enumerate() {
-        let new_token_stream = if let Some(inner_type) = get_generic_inner_type(
-            ty, "Option") {
+        let new_token_stream = if let Some(inner_type) = get_generic_inner_type(ty, "Option") {
             quote::quote!(
                 fn #ident(&mut self, #ident: #inner_type) -> &mut Self {
                     self.#ident = std::option::Option::Some(#ident);
                     self
                 }
             )
-        } else if let Some(ref user_specified_ident) = get_field_user_specified_attrs(&fields[idx])? {
-            let inner_type = get_generic_inner_type(ty, "Vec").ok_or(
-                syn::Error::new(fields[idx].span(), "'each' field must be a Vec type"))?;
+        } else if let Some(ref user_specified_ident) = get_field_user_specified_attrs(&fields[idx])?
+        {
+            let inner_type = get_generic_inner_type(ty, "Vec").ok_or(syn::Error::new(
+                fields[idx].span(),
+                "'each' field must be a Vec type",
+            ))?;
 
             let mut ts_piece = quote::quote!(
                 fn #user_specified_ident(&mut self, #user_specified_ident: #inner_type) -> &mut Self {
@@ -161,21 +168,20 @@ fn generate_struct_setter(st: &syn::DeriveInput) -> syn::Result<proc_macro2::Tok
 
 fn generate_struct_build_fn(st: &syn::DeriveInput) -> syn::Result<proc_macro2::TokenStream> {
     let fields = get_fields_from_derive_input(st)?;
-    let tys: Vec<_> = fields.iter().map(|f|&f.ty).collect();
+    let tys: Vec<_> = fields.iter().map(|f| &f.ty).collect();
 
     let mut check_fields_pieces = Vec::new();
     for idx in 0..fields.len() {
         let ident = &fields[idx].ident.clone().unwrap();
-        if get_generic_inner_type(tys[idx], "Option").is_none() &&
-            get_generic_inner_type(tys[idx], "Vec").is_none() {
-            check_fields_pieces.push(
-                quote::quote!(
-                    if self.#ident.is_none() {
-                        let err = format!("{} field is missing", stringify!(#ident));
-                        return std::result::Result::Err(err.into());
-                    }
-                )
-            );
+        if get_generic_inner_type(tys[idx], "Option").is_none()
+            && get_generic_inner_type(tys[idx], "Vec").is_none()
+        {
+            check_fields_pieces.push(quote::quote!(
+                if self.#ident.is_none() {
+                    let err = format!("{} field is missing", stringify!(#ident));
+                    return std::result::Result::Err(err.into());
+                }
+            ));
         }
     }
 
@@ -183,23 +189,17 @@ fn generate_struct_build_fn(st: &syn::DeriveInput) -> syn::Result<proc_macro2::T
     for idx in 0..fields.len() {
         let ident = &fields[idx].ident.clone().unwrap();
         if get_field_user_specified_attrs(&fields[idx])?.is_some() {
-            fill_fields.push(
-                quote::quote!(
-                    #ident: self.#ident.clone()
-                )
-            );
+            fill_fields.push(quote::quote!(
+                #ident: self.#ident.clone()
+            ));
         } else if get_generic_inner_type(tys[idx], "Option").is_none() {
-            fill_fields.push(
-                quote::quote!(
-                    #ident: self.#ident.clone().unwrap()
-                )
-            );
+            fill_fields.push(quote::quote!(
+                #ident: self.#ident.clone().unwrap()
+            ));
         } else {
-            fill_fields.push(
-                quote::quote!(
-                    #ident: self.#ident.clone()
-                )
-            );
+            fill_fields.push(quote::quote!(
+                #ident: self.#ident.clone()
+            ));
         }
     }
 
@@ -221,12 +221,18 @@ fn generate_struct_build_fn(st: &syn::DeriveInput) -> syn::Result<proc_macro2::T
 
 #[allow(dead_code)]
 fn get_optional_inner_type(t: &syn::Type) -> Option<&syn::Type> {
-    if let syn::Type::Path(syn::TypePath { path: syn::Path { segments, .. }, .. }) = t {
+    if let syn::Type::Path(syn::TypePath {
+        path: syn::Path { segments, .. },
+        ..
+    }) = t
+    {
         if let Some(last_seg) = segments.last() {
             if last_seg.ident.to_string() == "Option".to_string() {
-                if let syn::PathArguments::AngleBracketed(
-                    syn::AngleBracketedGenericArguments{
-                        args, .. }) = &last_seg.arguments {
+                if let syn::PathArguments::AngleBracketed(syn::AngleBracketedGenericArguments {
+                    args,
+                    ..
+                }) = &last_seg.arguments
+                {
                     if let Some(syn::GenericArgument::Type(t)) = args.last() {
                         return Some(t);
                     }
@@ -247,16 +253,20 @@ fn get_field_user_specified_attrs(field: &syn::Field) -> syn::Result<Option<syn:
             } else {
                 return Ok(None);
             }
-            if let syn::Expr::Assign(syn::ExprAssign{left, right, ..}) = &v {
+            if let syn::Expr::Assign(syn::ExprAssign { left, right, .. }) = &v {
                 if left.into_token_stream().to_string() == "each" {
-                    if let Expr::Lit(syn::ExprLit{lit: syn::Lit::Str(l), ..}) = right.deref() {
-                        return Ok(Some(syn::Ident::new(
-                            l.value().as_ref(),
-                            attr.span())));
+                    if let Expr::Lit(syn::ExprLit {
+                        lit: syn::Lit::Str(l),
+                        ..
+                    }) = right.deref()
+                    {
+                        return Ok(Some(syn::Ident::new(l.value().as_ref(), attr.span())));
                     }
                 } else {
-                    return Err(syn::Error::new_spanned(attr.meta.to_token_stream(),
-                                                       r#"expected `builder(each = "...")`"#));
+                    return Err(syn::Error::new_spanned(
+                        attr.meta.to_token_stream(),
+                        r#"expected `builder(each = "...")`"#,
+                    ));
                 }
             }
         } else {
@@ -267,12 +277,18 @@ fn get_field_user_specified_attrs(field: &syn::Field) -> syn::Result<Option<syn:
 }
 
 fn get_generic_inner_type<'a>(t: &'a syn::Type, outer_ident_name: &str) -> Option<&'a syn::Type> {
-    if let syn::Type::Path(syn::TypePath { path: syn::Path { segments, .. }, .. }) = t {
+    if let syn::Type::Path(syn::TypePath {
+        path: syn::Path { segments, .. },
+        ..
+    }) = t
+    {
         if let Some(last_seg) = segments.last() {
             if last_seg.ident.to_string() == outer_ident_name.to_string() {
-                if let syn::PathArguments::AngleBracketed(
-                    syn::AngleBracketedGenericArguments{
-                        args, .. }) = &last_seg.arguments {
+                if let syn::PathArguments::AngleBracketed(syn::AngleBracketedGenericArguments {
+                    args,
+                    ..
+                }) = &last_seg.arguments
+                {
                     if let Some(syn::GenericArgument::Type(t)) = args.last() {
                         return Some(t);
                     }
--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,35 +1,87 @@
 use proc_macro::TokenStream;
 use quote::ToTokens;
-use syn::Field;
+use std::collections::HashMap;
+use syn::{parse_quote};
+use syn::visit::{self, Visit};
+use syn::spanned::Spanned;
+
+struct TypePathVisitor {
+    generic_type_names: Vec<String>, // 记录所有泛型参数的名字
+    associated_types: HashMap<String, Vec<syn::TypePath>>, // 记录所有满足条件的语法树节点
+}
+
+// 遍历语法树的节点
+impl<'ast> Visit<'ast> for TypePathVisitor {
+    fn visit_type_path(&mut self, node: &syn::TypePath) {
+        if node.path.segments.len() >= 2 {
+            let generic_type_name = node.path.segments[0].ident.to_string();
+            if self.generic_type_names.contains(&generic_type_name) {
+                self.associated_types.entry(generic_type_name).or_insert(Vec::new()).push(node.clone());
+            }
+        }
+
+        visit::visit_type_path(self, node);
+    }
+}
 
 #[proc_macro_derive(CustomDebug, attributes(debug))]
 pub fn derive(input: TokenStream) -> TokenStream {
     let st = syn::parse_macro_input!(input as syn::DeriveInput);
     match do_expand(&st) {
-        syn::Result::Ok(ret) => {
-            ret.into()
-        }
-        syn::Result::Err(e) => {
-            e.to_compile_error().into()
-        }
+        syn::Result::Ok(ret) => ret.into(),
+        syn::Result::Err(e) => e.to_compile_error().into(),
     }
 }
 
 fn do_expand(st: &syn::DeriveInput) -> syn::Result<proc_macro2::TokenStream> {
     let origin_struct_ident = &st.ident;
-    // let origin_struct_ident_literal = origin_struct_ident.to_string();
 
     let mut generic_data = st.generics.clone();
-    for g in generic_data.params.iter_mut() {
-        if let syn::GenericParam::Type(t) = g {
-            t.bounds.push(syn::parse_quote!(std::fmt::Debug));
+
+    if let Some(hatch) = get_struct_escape_hatch(st) {
+        generic_data.make_where_clause();
+        generic_data.where_clause.as_mut().unwrap().predicates.push(syn::parse_str(hatch.as_str()).unwrap());
+    } else {
+        // 构建两个列表，一个是PhantomData中使用到的泛型参数，另一个是输入结构体中所有的参数。
+        let fields = get_struct_all_fields(st)?;
+        let mut field_type_names = Vec::new();
+        let mut phantom_data_type_param_names = Vec::new();
+        for field in fields.iter() {
+            if let Some(s) = get_field_type_name(field)? {
+                field_type_names.push(s);
+            }
+            if let Some(s) = get_phantom_data_generic_type_name(field)? {
+                phantom_data_type_param_names.push(s);
+            }
+        }
+        let associated_types_map = get_generic_associated_types(st);
+        for g in generic_data.params.iter_mut() {
+            if let syn::GenericParam::Type(t) = g {
+                let type_param_name = t.ident.to_string();
+
+                if phantom_data_type_param_names.contains(&type_param_name)
+                    && !field_type_names.contains(&type_param_name) {
+                    continue;
+                }
+
+                if associated_types_map.contains_key(&type_param_name)
+                    && !field_type_names.contains(&type_param_name) {
+                    continue;
+                }
+
+                t.bounds.push(syn::parse_quote!(std::fmt::Debug));
+            }
+        }
+
+        generic_data.make_where_clause();
+        for (_, associated_types) in associated_types_map {
+            for associated_type in associated_types {
+                generic_data.where_clause.as_mut().unwrap().predicates.push(parse_quote!(#associated_type:std::fmt::Debug));
+            }
         }
     }
 
     let (impl_generics, type_generics, where_clause) = generic_data.split_for_impl();
-    // eprintln!("{:?}", impl_generics);
-    // eprintln!("{:?}", type_generics);
-    // eprintln!("{:?}", where_clause);
 
     let core_token_stream = generate_fmt_fn_core(st)?;
 
@@ -40,6 +92,7 @@ fn do_expand(st: &syn::DeriveInput) -> syn::Result<proc_macro2::TokenStream> {
             }
         }
     );
+    // eprintln!("{}", quote::quote!(#final_token_stream));
     syn::Result::Ok(final_token_stream)
 }
 
@@ -50,7 +103,7 @@ fn generate_fmt_fn_core(st: &syn::DeriveInput) -> syn::Result<proc_macro2::Token
         f.debug_struct(#origin_struct_ident_literal)
     );
 
-    if let syn::Data::Struct(syn::DataStruct{fields, ..}) = &st.data {
+    if let syn::Data::Struct(syn::DataStruct { fields, .. }) = &st.data {
         for field in fields {
             let mut format_str = "{:?}".to_string();
             if let Some(fs) = get_custom_format_of_field(field)? {
@@ -69,29 +122,29 @@ fn generate_fmt_fn_core(st: &syn::DeriveInput) -> syn::Result<proc_macro2::Token
     Ok(res_token_stream)
 }
 
-#[allow(dead_code)]
-fn get_struct_all_fields_ident(st: &syn::DeriveInput) -> syn::Result<Vec<&syn::Ident>> {
-    let mut fields_ident = Vec::new();
-    if let syn::Data::Struct(syn::DataStruct{fields, ..}) = &st.data {
-        for field in fields {
-            if let Some(ident) = &field.ident {
-                fields_ident.push(ident);
-            }
-        }
+fn get_struct_all_fields(st: &syn::DeriveInput) -> syn::Result<&syn::Fields> {
+    if let syn::Data::Struct(syn::DataStruct { fields, .. }) = &st.data {
+        return syn::Result::Ok(fields);
     }
-    return syn::Result::Ok(fields_ident)
+    return syn::Result::Err(syn::Error::new(st.span(), "should be a struct"));
 }
 
 fn get_custom_format_of_field(field: &syn::Field) -> syn::Result<Option<String>> {
     for attr in &field.attrs {
-        if let syn::Meta::NameValue(syn::MetaNameValue{path, value, .. }) = &attr.meta {
+        if let syn::Meta::NameValue(syn::MetaNameValue { path, value, .. }) = &attr.meta {
             if path.to_token_stream().to_string() == "debug" {
-                if let syn::Expr::Lit(syn::ExprLit{lit: syn::Lit::Str(l), ..}) = value {
+                if let syn::Expr::Lit(syn::ExprLit {
+                    lit: syn::Lit::Str(l),
+                    ..
+                }) = value
+                {
                     return Ok(Some(l.value()));
                 }
             } else {
-                return Err(syn::Error::new_spanned(attr.meta.to_token_stream(),
-                                                   r#"expected `#[debug = "..."]`"#));
+                return Err(syn::Error::new_spanned(
+                    attr.meta.to_token_stream(),
+                    r#"expected `#[debug = "..."]`"#,
+                ));
             }
         } else {
             return Ok(None);
@@ -101,12 +154,77 @@ fn get_custom_format_of_field(field: &syn::Field) -> syn::Result<Option<String>>
     Ok(None)
 }
 
-// fn get_phantom_data_generic_type_name(field: &syn::Field) -> syn::Result<Option<String>> {
-//     if let syn::Type::Path(syn::TypePath{path: syn::Path{segments, ..}, ..}) = &field.ty {
-//         if let Some(syn::PathSegment{ident, arguments}) = segments.last() {
-//
-//         }
-//     }
-//
-//     Ok(None)
-// }
+fn get_phantom_data_generic_type_name(field: &syn::Field) -> syn::Result<Option<String>> {
+    if let syn::Type::Path(syn::TypePath {
+        path: syn::Path { segments, .. },
+        ..
+    }) = &field.ty
+    {
+        if let Some(syn::PathSegment { ident, arguments }) = segments.last() {
+            if ident == "PhantomData" {
+                if let syn::PathArguments::AngleBracketed(syn::AngleBracketedGenericArguments {
+                    args,
+                    ..
+                }) = arguments
+                {
+                    if let Some(syn::GenericArgument::Type(syn::Type::Path(type_path))) =
+                        args.first()
+                    {
+                        if let Some(generic_ident) = type_path.path.segments.first() {
+                            return Ok(Some(generic_ident.ident.to_string()));
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    Ok(None)
+}
+
+fn get_field_type_name(field: &syn::Field) -> syn::Result<Option<String>> {
+    if let syn::Type::Path(syn::TypePath {
+        path: syn::Path { segments, .. },
+        ..
+    }) = &field.ty
+    {
+        if let Some(syn::PathSegment { ident, .. }) = segments.last() {
+            return Ok(Some(ident.to_string()));
+        }
+    }
+    Ok(None)
+}
+
+fn get_generic_associated_types(st: &syn::DeriveInput) -> HashMap<String, Vec<syn::TypePath>> {
+    let origin_generic_param_names: Vec<String> = st.generics.params.iter().filter_map(|f| {
+        if let syn::GenericParam::Type(ty) = f {
+            return Some(ty.ident.to_string());
+        }
+        return None;
+    }).collect();
+
+    let mut visitor = TypePathVisitor{
+        generic_type_names: origin_generic_param_names,
+        associated_types: HashMap::new(),
+    };
+
+    visitor.visit_derive_input(st);
+    return visitor.associated_types;
+}
+
+fn get_struct_escape_hatch(st: &syn::DeriveInput) -> Option<String> {
+    if let Some(inert_attr) = st.attrs.first() {
+        if inert_attr.meta.path().segments.first().unwrap().ident.to_string() == "debug" {
+            if let Ok(syn::Expr::Assign(syn::ExprAssign{left, right, ..})) = inert_attr.parse_args::<syn::Expr>() {
+                if let syn::Expr::Path(syn::ExprPath{path: syn::Path{segments, ..}, ..}) = *left {
+                    if segments[0].ident.to_string() == "bound" {
+                        if let syn::Expr::Lit(syn::ExprLit{lit: syn::Lit::Str(str), ..}) = *right {
+                            return Some(str.value());
+                        }
+                    }
+                }
+            }
+        }
+    }
+    None
+}
