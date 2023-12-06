3e161e53f57852e6053f982f47fce160e3fffc9d
‘xianhanliu’ <liuxianhan@vip.qq.com>
2023-12-06 23:34:37 +0800

    完成所有题目，以后需要好好思考，多写写过程宏

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,6 +1,8 @@
 use proc_macro::TokenStream;
 use quote::ToTokens;
-use syn::{ExprMatch, Path};
+use syn::{ExprMatch};
+use syn::spanned::Spanned;
+use syn::visit_mut::VisitMut;
 
 #[proc_macro_attribute]
 pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
@@ -49,9 +51,9 @@ fn check_enum_order(item_enum: &syn::ItemEnum) -> syn::Result<proc_macro2::Token
 pub fn check(args: TokenStream, input: TokenStream) -> TokenStream {
     let _ = args;
     // check会作用于一个函数上
-    let input_item = syn::parse_macro_input!(input as syn::ItemFn);
+    let mut input_item = syn::parse_macro_input!(input as syn::ItemFn);
 
-    match do_match_expand(&input_item) {
+    match do_match_expand(&mut input_item) {
         Ok(token_stream) => token_stream.into(),
         Err(err) => {
             let mut e = err.to_compile_error();
@@ -61,11 +63,22 @@ pub fn check(args: TokenStream, input: TokenStream) -> TokenStream {
     }
 }
 
-fn do_match_expand(item_fn: &syn::ItemFn) -> syn::Result<proc_macro2::TokenStream> {
-    Ok(proc_macro2::TokenStream::new())
+fn do_match_expand(item_fn: &mut syn::ItemFn) -> syn::Result<proc_macro2::TokenStream> {
+    let mut visitor = MatchVisitor{err:None};
+    visitor.visit_item_fn_mut(item_fn);
+    match &visitor.err {
+        None => {
+            syn::Result::Ok(item_fn.to_token_stream())
+        },
+        Some(e) => {
+            syn::Result::Err(e.clone().into())
+        }
+    }
 }
 
-struct MatchVisitor {}
+struct MatchVisitor {
+    err: Option<syn::Error>,
+}
 
 impl syn::visit_mut::VisitMut for MatchVisitor {
     fn visit_expr_match_mut(&mut self, expr_match: &mut ExprMatch) {
@@ -85,17 +98,45 @@ impl syn::visit_mut::VisitMut for MatchVisitor {
                 return;
             },
             Some(idx) => {
-                let mut match_arm_names = Vec::new();
+                expr_match.attrs.remove(idx);
+
+                let mut match_arm_names: Vec<(String, &dyn ToTokens)> = Vec::new();
 
                 for arm in &expr_match.arms {
-                    match arm {
-
+                    match &arm.pat {
+                        syn::Pat::Path(p) => {
+                            match_arm_names.push((trans_path_to_string(&p.path), &p.path));
+                        },
+                        syn::Pat::TupleStruct(p)=>{
+                            match_arm_names.push((trans_path_to_string(&p.path), &p.path));
+                        },
+                        syn::Pat::Struct(p)=>{
+                            match_arm_names.push((trans_path_to_string(&p.path), &p.path));
+                        },
+                        syn::Pat::Ident(p) => {
+                            match_arm_names.push((p.ident.to_string(), &p.ident));
+                        },
+                        syn::Pat::Wild(p) => {
+                            match_arm_names.push(("_".to_string(), &p.underscore_token));
+                        },
+                        _ => {
+                            self.err = Some(syn::Error::new(arm.pat.span(), "unsupported by #[sorted]"));
+                            return;
+                        }
                     }
-                    match_arm_names.push(arm.clone());
                 }
-                match_arm_names.sort_by(|arm| {
-                    arm.pat
+                let mut sorted_arm_names = match_arm_names.clone();
+                sorted_arm_names.sort_by(|a, b| {
+                    a.0.cmp(&b.0)
                 });
+                for (a, b) in match_arm_names.iter().zip(sorted_arm_names.iter()) {
+                    if a.0 != b.0 {
+                        self.err = Some(syn::Error::new_spanned(b.1, format!("{} should sort before {}", b.0, a.0)));
+                        return;
+                    }
+                }
+                syn::visit_mut::visit_expr_match_mut(self, expr_match);
+                return;
             }
         }
     }
